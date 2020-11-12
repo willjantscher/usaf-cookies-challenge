950a3e77f0aebf5d8edd8a60b6e6da21b3d6caf3
willjantscher <william.jantscher@gmail.com>
2020-11-12 08:19:34 -0700

    added another example where it is not hard coded

--- app.js
+++ app.js
@@ -11,29 +11,72 @@
 const express = require('express')
 var cookieParser = require('cookie-parser')
 
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3009
 
+app.use(bodyParser.json())
 app.use(cookieParser())
 //--------------------------------------------------------------------------------------------
 
 
+// create a route to /login with their name and set a cookie
 
-app.get('/login', (req, res) => {
-        //set parameters for the cookies
-        var opts = {
-            maxAge: 900000,
-            httpOnly: true
-        };
-        res.cookie('firstName','General', opts);
-        res.cookie('lastName','Kenobi', opts);
-        res.end();
+app.post('/login', (req, res) => {
+    //send over user data in body of request
+    let user = req.body;
+    let result;
+    if(user.name) {
+        result = {
+            "status": "Success",
+            "message": "You're logged in"
+        }
+    }
+    else {
+    result = {
+        "status": "Failure",
+        "message": "You have not logged in"
+        }
+    res.status(404).send(result)    //sets status and logs result
+    }
+    res.cookie('name', user.name);
+    res.json(result);
 })
+// POST http://localhost:3009/login
+// {
+//     "name": "william"
+// }
 
 app.get('/hello', (req, res) => {
-    console.log('cookies: ', req.cookies)
-    res.send('Hello there! ... ' + req.cookies.firstName + ' ' + req.cookies.lastName)
+    let name = req.cookies.name
+    if(name) {
+        res.send(`Welcome, ${name}!`)
+    }
+    else {
+        res.send('Not logged in')
+    }
 })
+// GET http://localhost:3009/hello
+
+
+
+
+
+//hard coded setting of cookies
+// app.get('/login', (req, res) => {
+//         //set parameters for the cookies
+//         var opts = {
+//             maxAge: 900000,
+//             httpOnly: true
+//         };
+//         res.cookie('firstName','General', opts);
+//         res.cookie('lastName','Kenobi', opts);
+//         res.end();
+// })
+// app.get('/hello', (req, res) => {
+//     console.log('cookies: ', req.cookies)
+//     res.send('Hello there! ... ' + req.cookies.firstName + ' ' + req.cookies.lastName)
+// })
 
 
 
